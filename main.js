48208e72dd71c29e5632556c8929aa9b7525da60
lfrestrepo004sena <70179712+lfrestrepo004sena@users.noreply.github.com>
2022-03-24 11:05:52 -0500

    Commit inicial

---
+++ main.js
@@ -0,0 +1,20 @@
+//Suma
+
+function suma(){
+    var a = 0;
+    var b = 0;
+    var result = 0;
+    //Convertir strings en enteros
+    a = parseInt(prompt("POR FAVOR INGRESE EL PRIMER VALOR A SUMAR"));
+    b = parseInt(prompt("POR FAVOR INGRESE EL SEGUNDO VALOR A SUMAR"));
+    
+    result = a + b;
+    
+    alert("EL RESULTADO DE LA SUMA ES: " + result);
+}
+
+
+
+
+
+
